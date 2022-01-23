d00b200d822081a9e3f8242a5e8e836c9adcb0ca
ayoubarouche <aarouche2016@gmail.com>
2022-01-23 19:57:01 +0100

    adding the mc/dc tests still need some fixes

--- mcdc.py
+++ mcdc.py
@@ -3,16 +3,15 @@
 import ast
 from typing import final
 
-import itertools
 
-from numpy import array
+
 
 # get all the variable names in the expression : 
 generale_result = []
 
 
 def get_variables(expression):
-    variables = [node.id  for node in ast.walk(ast.parse(formula))
+    variables = [node.id  for node in ast.walk(ast.parse(expression))
              if isinstance(node , ast.Name)
              ]
     variables = list(dict.fromkeys(variables))
@@ -56,18 +55,6 @@ def get_same_test_case(all_the_test_cases  , test_case , variable_to_change ):
     return same_test_cases
             
             
-def remove_duplicate_valid_test_cases (valid_test_cases_of_all_variabels):
-    final_test_cases = []
-    merged_test_cases = []
-    for valid_test_case in valid_test_cases_of_all_variabels:
-        # for each variable : 
-        result  = list(itertools.chain.from_iterable(valid_test_case)) # merge all the test cases :
-        print("inside the merging algorithm : ")
-        
-        result = remove_duplicates_from_list(result)
-        merged_test_cases.append(result)
-        print(result)
-    return merged_test_cases
 
 def remove_duplicates_from_list(test_cases):
     result = []
@@ -75,32 +62,84 @@ def remove_duplicates_from_list(test_cases):
         if test_case not in result :
             result.append(test_case)
     return result
-def get_mcdc(valid_test_cases) :
-    mcdcs = []
-    test_cases = remove_duplicate_valid_test_cases(valid_test_cases)
+
+def verify_and_add_test_case(test_cases) :
+    final_test_cases = []
+    length_test_varibles = len(test_cases)
+    i = 0
+    check = False
+    for i in range(length_test_varibles) :
+        check=True
+
+        for part in test_cases[i] :
+            
+                for k in range(i , length_test_varibles-1):
+                    if check and verify_one_of_two_test_cases_already_exist(part , test_cases[k+1]):
+                        final_test_cases.append(part)
+                        check=False
+                    
     
-    for i in range(len(valid_test_cases)) :
-        for j in range(len(test_cases)):
-            if(i==j):
-                continue
-            test_cases2 = [x for x in test_cases[i] if x in test_cases[j]]
-        print("the value is : ")
-        print(test_cases2)
-        mcdcs.append(test_cases2)
-    return mcdcs   
-    
+    # last_test_cases_to_add = check_if_a_variable_contains_two_of_test_cases(final_result , test_cases[length_test_varibles-1])
+
+    final_test_cases.append(test_cases[length_test_varibles-1][0])
+    final_result = remove_duplicates_from_list(change_from_nested_lists_to_one_list(final_test_cases))
+    return final_result
+
+def check_if_a_variable_contains_two_of_test_cases(test_cases_to_verify , variable_test_case):
+    for i in range(len(test_cases_to_verify)):
+        for j in range(i , len(test_cases_to_verify)-1):
+            for test_cases_part in variable_test_case :
+                if test_cases_part == [test_cases_to_verify[i] , test_cases_to_verify[j+1]]:
+                    return  [test_cases_to_verify[i] , test_cases_to_verify[j+1]]
+    return []
+            
+def verify_one_of_two_test_cases_already_exist(pair_of_test_cases , test_cases_to_verify):
+    first_test_case = pair_of_test_cases[0]
+    secand_test_case = pair_of_test_cases[1]
+    # verify for the first transaction  : 
+
+    for test_cases_to_verify_part in test_cases_to_verify :
+        if check_if_exist(first_test_case,test_cases_to_verify_part) :
+            return True
+    for test_cases_to_verify_part in test_cases_to_verify :
+        if check_if_exist(secand_test_case,test_cases_to_verify_part) : 
+            return True
+   
+    return False
+
+def formating_output(test_cases , variabels):
+    print("number      ", end="")
+    for variable in variabels :
+        print(variable+"          " , end="")
+    print("result")
+    print("-----------------------------------------")
+    for test_case in test_cases :
+        print(all_test_cases_with_results_with_id[str(test_case)],end="           ")
+        for variable in variables :
+            print(str(test_case[variable])+"     " , end="")
+        print(test_case["result"])
+        print("--------------------------------------------")    
 def check_if_exist(test_case , test_cases):
-    return (test_case in test_cases)
-# def generate_lests_from_dict(dict):
-#     result = []
-#     result.append(dict)
-#     result.append(dict[])
+    for test_case_part in test_cases : 
+        if test_case==test_case_part:
+            return True
+    return False
+
+def change_from_nested_lists_to_one_list(list):
+    final_tests = []
+    for test_case in list :
+        for nested_test_case in test_case :
+            final_tests.append(nested_test_case)
+    return final_tests
 if __name__ == "__main__":
     all_test_cases_with_results = []
-    formula = 'a & (b | c)'
+    
+    formula = input("entrer une expression :")
+    formula = formula.replace('&&', ' and ').replace('||', ' or ').replace('!', ' not ')
     variables = get_variables(formula)
+    print("les test case names sont : ",end="")
     print(variables)
-    print(len(variables))
+
     # generating all the test cases that we should do 
     generate_all_test_cases(len(variables) , 0 , [])
     
@@ -113,65 +152,41 @@ if __name__ == "__main__":
         result_copy = result.copy()
         #print(result_copy)
         all_test_cases_with_results.append(result)
+    all_test_cases_with_results_with_id={}
+    id= 1 
+    for test_case in all_test_cases_with_results :
+        all_test_cases_with_results_with_id[str(test_case)] = id
+        id+=1
     general_valid_test_cases_for_each_variable = []
-    print("before the values : ")
+   
     for variable in variables :
         general_valid_test_cases_for_a_variable = []
         general_valid_test_cases_result = []
-        print("for variable "+ variable)
+        
         for test_case in all_test_cases_with_results :
             valid_opposite_test_case = get_same_test_case(all_test_cases_with_results  , test_case.copy() , variable)
             if len(valid_opposite_test_case) == 1 : 
                 continue
             general_valid_test_cases_for_a_variable.append(valid_opposite_test_case)
-            print(valid_opposite_test_case)
-        general_valid_test_cases_result = general_valid_test_cases_for_a_variable[0:int(len(general_valid_test_cases_for_a_variable)/2)]
+        general_valid_test_cases_result = general_valid_test_cases_for_a_variable[0:int(len(general_valid_test_cases_for_a_variable)/2)]   
         general_valid_test_cases_for_each_variable.append(general_valid_test_cases_result.copy())
     
-        print("the size is : "+str(len(general_valid_test_cases_result)))
-        print("==========================================================")
-        print(general_valid_test_cases_result)
-        print("==================================================")
-    # print("after the values : ")
-    # print(len(general_valid_test_cases_for_a_variable))
-
-    # print("after remving test cases : ")
-    # print(len(general_valid_test_cases_for_a_variable))
     final_test_cases = []
-    print("the general values is : ")
-    print(general_valid_test_cases_for_each_variable)
-    # final_result = remove_duplicate_valid_test_cases (general_valid_test_cases_for_a_variable)
+
+   
     
-    print("getting mcdcs : ")
-    length = len(general_valid_test_cases_for_each_variable)
-    for i in range(length) :
-        variable_test_cases  = general_valid_test_cases_for_each_variable[i]
-        for j in range(len(variable_test_cases)):
-            for k in range(2) :
-                ## algo katchof ila kan 4ir wahd element mn l pair f test cases lakhrin kat2ajoutihom bjoj o kadoz l variable lakher
-                	
-        
+    print("all the test cases with out MC/CD are : ")
     
-    # for valid_cases_for_variable in general_valid_test_cases_for_each_variable:
-    #     print("for a variable : ")
-    #     print(valid_cases_for_variable)    
-    # print("the final result is : ")
-    # print("the lenght is "+ str(len(final_result)))
-    # for test_case in final_result :
-    #     print(test_case)
-   # print()
-    ## applicating the mcdc of our test cases : 
+    formating_output(all_test_cases_with_results , variables)
+    print("general test cases for each variable : ")
+    for i in range(len(general_valid_test_cases_for_each_variable)):
+        print("for variable : "+variables[i])
+        for j in range(len(general_valid_test_cases_for_each_variable[i])):
+            print("the part number : "+ str(j+1))
+            formating_output(general_valid_test_cases_for_each_variable[i][j] , variables)
+    print("all the test cases or MC/CD are : ")
     
-    # assign_values_to_variables(variables , values)
-    # result = evaluate_expression(formula)
-    # print("result is :  "+str(result))
-    # for i in variables :
-    #     print("the variable name is : " +str(i))
+    final_result = verify_and_add_test_case(general_valid_test_cases_for_each_variable)
     
-    # range = locals()
-    # for i in range :
-    #     print("the local name is : "+locals[])
-    # print(names)
-    # locals()['k'] = 3
-    # y = function_expression(k ,2)
-    # print("the value of is y is : "+str(y))
+    formating_output(final_result , variables)
+    
